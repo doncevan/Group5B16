8bb2ab6cdc88f44fcb4e69fb727ef5fbf9cfef32
oksanaZadvorna <oksanazadvorna9@gmail.com>
2023-07-12 18:02:08 -0500

    the common methods and pom.xml file are ready to be deployed

---
+++ src/test/java/utils/CommonMethods.java
@@ -0,0 +1,113 @@
+package utils;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+
+public class CommonMethods {
+
+    public static WebDriver driver;
+
+    public static void openBrowserAndNavigateToURL() {
+        ConfigReader.readProperties(Constants.CONFIG_READER_PATH);
+        switch (ConfigReader.getPropertyValue("browser")) {
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+        }
+        driver.manage().window().maximize();
+        driver.get(ConfigReader.getPropertyValue("url"));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        
+    }
+
+    public static void closeBrowser() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    public static void sendText(String text, WebElement element) {
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public static WebDriverWait getWait(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        return wait;
+    }
+
+    public static void waitForClickability(WebElement element){
+        getWait().until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public static void click(WebElement element){
+        waitForClickability(element);
+        element.click();
+    }
+
+    //select from dropdown by text
+    public static void selectFromDropdown(WebElement dropDown, String visibleText) {
+        Select sel = new Select(dropDown);
+        sel.selectByVisibleText(visibleText);
+    }
+
+    //select from dropdown by index
+    public static void selectFromDropdown(WebElement dropDown, int index) {
+        Select sel = new Select(dropDown);
+        sel.selectByIndex(index);
+    }
+
+    //select from dropdown by value
+    public static void selectFromDropdown(String value, WebElement dropDown) {
+        Select sel = new Select(dropDown);
+        sel.selectByValue(value);
+    }
+
+    public static byte[] takeScreenshot(String fileName){
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        //we write this line because cucumber accepts array of byte for screenshot
+        byte[] picBytes = ts.getScreenshotAs(OutputType.BYTES);
+        File screenShot = ts.getScreenshotAs(OutputType.FILE);
+        //in case if it doesn't find file name or path it will throw an exception
+
+        try{
+            FileUtils.copyFile(screenShot,
+                    new File(Constants.SCREENSHOT_FILEPATH + fileName+" "
+                            +getTimeStamp("yyyy-MM-dd-HH-mm-ss")+".png"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return picBytes;
+    }
+
+    public static String getTimeStamp(String pattern){
+        //it returns the current date and time in java
+        Date date = new Date();
+        //this fubction sdf used to format the date as per the pattern we are passing
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        //this line returns the formated data
+        return sdf.format(date);
+    }
+
+
+
+
+}
