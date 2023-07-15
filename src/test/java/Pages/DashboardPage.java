13bf42935729563620855d5aff1506dcea792d68
FekaduSerbesa <fike2006@yahoo.com>
2023-07-14 22:09:57 -0400

    user story G-15 completed

--- src/test/java/Pages/DashboardPage.java
+++ src/test/java/Pages/DashboardPage.java
@@ -12,7 +12,19 @@ public class DashboardPage extends CommonMethods {
 
     @FindBy(id="menu_pim_viewEmployeeList")
     public WebElement pimEmployeeBtn;
+    @FindBy(id="menu_admin_viewAdminModule")
+    public WebElement AdminMenuBtn;
+    @FindBy(id="menu_admin_Qualifications")
+    public WebElement adminQualificationBtn;
 
+    @FindBy(id="btnAdd")
+    public WebElement addBtn;
+    @FindBy(id="language_name")
+    public WebElement nameBtn;
+    @FindBy(id="btnSave")
+    public WebElement saveBtn;
+    @FindBy(id="menu_admin_viewLanguages")
+    public WebElement languagesBtn;
     public DashboardPage() {
         PageFactory.initElements(driver, this);
     }
