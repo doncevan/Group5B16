5e964b30fb516b29e7dca481741cff410a3e04b2
eksdeemephisto <shuyinvenus@gmail.com>
2023-07-15 14:14:27 -0400

    Final Update 7/15/2023 2:15 PM

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -8,6 +8,6 @@ public class Constants {
 
     public static final String EXCEL_READER_PATH = System.getProperty("user.dir")+"/src/test/resources/testData/batch16ExcellFile.xlsx";
 
-    public static final String PROFILE_PICTURE_PATH = System.getProperty("user.dir")+"/ProfilePic.jpg";
+    public static final String PROFILE_PICTURE_PATH = System.getProperty("user.dir")+"/src/test/resources/profilepic/ProfilePic.jpg";
 
 }
