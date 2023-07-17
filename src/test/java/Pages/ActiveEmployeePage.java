42cbebcb6f72b2a18f46c662f33a786014d4c2ca
engraciaV <edvallecillo96@gmail.com>
2023-07-16 21:15:47 -0400

    final active employee list

--- src/test/java/Pages/ActiveEmployeePage.java
+++ src/test/java/Pages/ActiveEmployeePage.java
@@ -8,24 +8,22 @@ import utils.CommonMethods;
 public class ActiveEmployeePage extends CommonMethods {
     @FindBy(xpath="//*[@id='txtUsername']")
     public WebElement usernameField;
-
     @FindBy(id="txtPassword")
     public WebElement passwordField;
-
     @FindBy(id="btnLogin")
     public WebElement loginButton;
-
     @FindBy(id="menu_pim_viewEmployeeList")
     public WebElement viewEmpList;
+    @FindBy(id="menu_pim_viewPimModule")
+    public WebElement viewPIM;
+    @FindBy(xpath="//select[@id='empsearch_employee_status']")
+    public WebElement empStatusDropDown;
     @FindBy(xpath="//option[@value='17']")
-    public WebElement activeDropDown;
+    public WebElement selectActiveEmp;
     @FindBy(xpath="//input[@id='searchBtn']")
     public WebElement searchBtn;
     @FindBy(xpath="//tbody")
     public WebElement activeEmpList;
-
-    @FindBy(xpath="//input[@id='ohrmList_chkSelectAll']")
-    public WebElement selectBox;
 
     public ActiveEmployeePage() {
         PageFactory.initElements(driver, this);
